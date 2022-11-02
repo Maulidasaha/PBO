b1712e9d4bf0fa60acf25b0cc34cfbe94b0d9b4f
Maulidasaha <116964454+Maulidasaha@users.noreply.github.com>
2022-11-02 04:54:54 -1200

    Add files via upload

---
+++ SOAL2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package source.code;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SOAL2 {
+  public static void main (String [] args) {
+      String nama = "MAULIDA HASANAH";
+      String program_studi = "Pemrograman Berorienrasi Objek";
+      String nama_fakultas = "Institut Teknologi Sapta Mandiri";
+        System.out.println("Nama\t\t: "+nama);
+        System.out.println("Program Studi\t: "+program_studi);
+        System.out.println("Nama Fakultas\t: "+nama_fakultas);
+    }
+}
